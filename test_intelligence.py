3317a8ccd753e9dcc4ca598182db402721b4d04d
Shuayb Warsame <shuaybwar@gmail.com>
2023-03-07 10:39:19 +0100

    Test cases

--- test_intelligence.py
+++ test_intelligence.py
@@ -2,13 +2,13 @@
 from intelligence import Intelligence
 
 def test_ai_level():
-    intelligence = Intelligence(ai_level=0, data_file='data.txt')
-    assert intelligence.get_ai_level() == 0
+    intelligence = Intelligence(ai_level=1, data_file='dice_values.csv')
+    assert intelligence.get_ai_level() == 1
 
 def test_easy_level():
-    intelligence = Intelligence(ai_level=0, data_file='data.txt')
+    intelligence = Intelligence(ai_level=1, data_file='dice_values.csv')
     assert intelligence.easy_level() in [0, 1]
 
 def test_hard_level():
-    intelligence = Intelligence(ai_level=1, data_file='data.txt')
+    intelligence = Intelligence(ai_level=1, data_file='dice_values.csv')
     assert isinstance(intelligence.hard_level(), int)

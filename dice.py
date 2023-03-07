748fcde54ddcf4c0a95a07dedf6f7340214badff
hangman <alkatebmohammed383@gmail.com>
2023-03-07 09:23:49 +0100

    Pylint

--- dice.py
+++ dice.py
@@ -1,10 +1,14 @@
+"""" import random from the standard library """
 import random
 
 
 class Dice:
-
-    def __init__(self):
-        pass
+    """
+    A class that represents a standard six-sided dice.
+    """
 
     def get_dice_cast(self) -> int:
+        """
+        returns random number between 1 and 6
+        """
         return random.randint(1, 6)
--- dice_hand.py
+++ dice_hand.py
@@ -1,17 +1,33 @@
+"""" import list, dice """
 from typing import List
 from dice import Dice
 
 
 class Dice_hand:
+    """
+    This class represents the dice hand of the player
+    It contains a dice object to roll dice, dice values, and the total of these values
+    """
     def __init__(self):
+        """
+        constructor that contain class instance variables dice_object, dice_values, and total_values
+        """
         self.__dice_object = Dice()
         self.__dice_values: List[int] = []
         self.__total_values = 0
 
     def get_total_values(self):
+        """
+        :return: dice hand total_values
+        """
         return self.__total_values
 
     def get_multiple_cast(self) -> int:
+        """
+        get dice cast and reset the values if the cast equal to 1
+        otherwise, add the cast value to the dice hand and increase total values
+        :return: cast
+        """
         cast = self.__dice_object.get_dice_cast()
         if cast == 1:
             self.__dice_values.clear()
@@ -22,5 +38,8 @@ class Dice_hand:
         return cast
 
     def reset_values(self):
+        """
+        reset dice_hand
+        """
         self.__dice_values: List[int] = []
         self.__total_values = 0

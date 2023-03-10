b7d78290e41a7eaac0a78c8a246af6488908a49a
hangman <alkatebmohammed383@gmail.com>
2023-03-10 09:47:34 +0100

    test_matchmaking

--- test_game.py
+++ test_game.py
@@ -1,7 +1,17 @@
 import sys
+import csv
+from typing import List
+from io import StringIO
+from unittest.mock import patch, MagicMock
+
+import pytest
+
+from dice import Dice
 from dice_hand import Dice_hand
 from player import Player
 from game import Game
+from histogram import Histogram
+from high_score import High_score
 
 
 def test_game_instance():
@@ -18,6 +28,10 @@ def test_list_import():
 
 
 def test_matchmaking():
-    """action = 2
-    name = "Mohammed"
-    assert name.isalpha()"""
+    with patch('builtins.input', side_effect=['2', 'player1', 'player2']):
+        game = Game()
+        game.players.append(Player("player1"))
+        game.players.append(Player("player2"))
+        assert len(game.players) == 2
+        assert game.players[0].name == 'player1'
+        assert game.players[1].name == 'player2'
